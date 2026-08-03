416c63db82bb082b7efae14c0c50283132a88228
Parsanov <romaparsanov@gmail.com>
2026-08-03 22:31:14 +0300

    add git ignore

---
+++ telegram_bot/DB/dp.py
@@ -0,0 +1,10 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from DB.model_user import Base
+
+engine = create_async_engine('sqlite+aiosqlite:///users.db')
+async_session = async_sessionmaker(engine, expire_on_commit=False)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
